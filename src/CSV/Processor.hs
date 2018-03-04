1e5607794776eb5dfbae6bac5ce3b39f9f0f3213
Art Harol <art.harol@bigtincan.com>
2018-03-04 23:42:03 +1100

    added more csv processing functionality

--- src/CSV/Processor.hs
+++ src/CSV/Processor.hs
@@ -1,19 +1,16 @@
-module CSV.Processor where
+module LearningHaskellDataAnalysis02 where
 
 import Data.List
+
 import Data.Either
 import Text.CSV
 import Database.HDBC
 import Database.HDBC.Sqlite3
+import Text.Regex.Posix ((=~))
 
-getColumnInCSV :: CSV -> String -> Either String Integer
-getColumnInCSV csv columnName =
-    case lookupResponse of
-        Nothing -> Left
-            "The column does not exist in the CSV file."
-        Just x -> Right (fromIntegral x) -- converts from Int to Num
-    where
-        lookupResponse = findIndex (== columnName) (head csv)
+average :: (Fractional a, Real b) => [b] -> a
+average xs = realToFrac (sum xs) / fromIntegral (length xs)
+
 
 -- Opens a CSV file and applies a function to a column
 -- Returns Either Error Message or the function result
@@ -28,6 +25,15 @@ applyToColumnInCSVFile func inFileName column = do
         (\csv -> applyToColumnInCSV func csv column) records
     where
         handleCSVError csv = Left "This does not appear to be a CSV file."
+
+getColumnInCSV :: CSV -> String -> Either String Integer
+getColumnInCSV csv columnName =
+    case lookupResponse of
+        Nothing -> Left
+            "The column does not exist in the CSV file."
+        Just x -> Right (fromIntegral x) -- converts from Int to Num
+    where
+        lookupResponse = findIndex (== columnName) (head csv)
 
 applyToColumnInCSV :: ([String] -> b) -> CSV -> String -> Either String b
 applyToColumnInCSV func csv column = either
@@ -90,3 +96,47 @@ convertCSVFileToSQL inFileName outFileName tableName fields = do
     where
         convertTool = convertCSVToSQL tableName outFileName fields
         handleCSVError csv = putStrLn "This does not apper to be a CSV file."
+
+countFieldsInEachRecord :: CSV -> [Integer]
+countFieldsInEachRecord = map genericLength
+
+lineNumbersWithIncorrectCount :: CSV -> [(Integer, Integer)]
+lineNumbersWithIncorrectCount (fields:csv) = filter
+        (\(_, thisCount) -> thisCount /= nfields)
+        lineNoCountPairs
+    where
+        nfields = genericLength fields
+        count = countFieldsInEachRecord csv
+        lineNoCountPairs = zip [1..] count
+
+identifyMatchingFields :: (String -> Bool) -> [String] -> [String] -> Integer -> [(String, String, String)]
+identifyMatchingFields myStringCmpFunc record headings idColumnIndex =
+    filter (\(_,_,field) -> myStringCmpFunc field) keyvalue
+    where
+        nfields = length headings
+        keyvalue = zip3 (replicate nfields (genericIndex record idColumnIndex)) headings record
+
+identifyInCSV :: (String -> Bool) -> CSV -> String -> Either String [(String, String, String)]
+identifyInCSV myFieldFunc csv idColumn = either
+    Left
+    (\ci -> Right $ concatMap (\rec -> identifyMatchingFields myFieldFunc rec (head csv) ci) (tail csv))
+    columnIndex
+    where
+        headings = head csv
+        columnIndex = getColumnInCSV csv idColumn
+
+identifyInCSVFile :: (String -> Bool) -> String -> String -> IO (Either String [(String, String, String)])
+identifyInCSVFile myStringCmpFunc inFileName idColumn = do
+    records <- parseCSVFromFile inFileName
+    return $ either
+        (\err -> Left "This does not appear to be a CSV file")
+        (\csv -> identifyInCSV myStringCmpFunc (init csv) idColumn)
+        records
+
+identifyInCSVFileFromColumn :: (String -> Bool) -> String -> String -> String -> IO (Either String [(String, String, String)])
+identifyInCSVFileFromColumn myRegexFunc inFileName idColumn desiredHeading = do
+    allFields <- identifyInCSVFile myRegexFunc inFileName idColumn
+    return $ either
+        Left
+        (\af -> Right $ filter (\(_, heading, _) -> heading == desiredHeading) af)
+        allFields
